79791d8a7ff4c1c0287496854803ba557fe4a3ea
Dave Gurnell <dave@untyped.com>
2016-09-08 13:57:29 +0100

    Better starting point for CSV

--- csv/src/main/scala/csv.scala
+++ csv/src/main/scala/csv.scala
@@ -1,11 +1,13 @@
+
+
+
 trait CsvEncoder[A] {
   def encode(value: A): List[String]
 }
 
-object CsvEncoder {
-  def apply[A](implicit encoder: CsvEncoder[A]): CsvEncoder[A] =
-    encoder
 
+
+object CsvEncoder {
   def pure[A](func: A => List[String]): CsvEncoder[A] =
     new CsvEncoder[A] {
       def encode(value: A): List[String] =
@@ -22,9 +24,15 @@ object CsvEncoder {
   //   pure(bool => List(if(bool) "yes" else "no"))
 }
 
+
+
 object Main extends Demo {
   def encodeCsv[A](value: A)(implicit enc: CsvEncoder[A]): List[String] =
     enc.encode(value)
 
+  // println(encodeCsv("Dave"))
+  // println(encodeCsv(123))
   // println(encodeCsv(true))
 }
+
+
